4186a446760472f8492b5c0e86d449227c2f4d71
letter5j <seanletter5j@Gmail.com>
2021-10-16 21:52:47 +0800

    1038. Binary Search Tree to Greater Sum Tree
    add solution

---
+++ 1038. Binary Search Tree to Greater Sum Tree/solution.py
@@ -0,0 +1,30 @@
+from typing import List, Optional, Dict, Set
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    sum: int = 0
+
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        tree = self.traverse(root)
+        return tree
+
+    def traverse(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return
+
+        tree = TreeNode()
+
+        tree.right = self.traverse(root.right)
+        self.sum = self.sum + root.val
+        tree.val = self.sum
+        tree.left = self.traverse(root.left)
+
+        return tree
