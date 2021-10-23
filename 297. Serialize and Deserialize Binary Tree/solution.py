32ce2c4be5d0c1a35511ba4fa1bcfd35a627af0d
letter5j <seanletter5j@Gmail.com>
2021-10-23 12:10:35 +0800

    297. Serialize and Deserialize Binary Tree
    add pre-order solution

--- 297. Serialize and Deserialize Binary Tree/solution.py
+++ 297. Serialize and Deserialize Binary Tree/solution.py
@@ -11,7 +11,7 @@ class TreeNode:
 
 class Codec:
     COMMA = ","
-    SEMICOLON = ";"
+    NULL = "#"
 
     def serialize(self, root: Optional[TreeNode]) -> str:
         """Encodes a tree to a single string.
@@ -28,7 +28,7 @@ class Codec:
 
         self.traverse(root, pre_order, in_order, post_order)
 
-        return self.SEMICOLON.join([self.COMMA.join(pre_order), self.COMMA.join(in_order)])
+        return self.COMMA.join(pre_order)
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
         """Decodes your encoded data to tree.
@@ -40,32 +40,33 @@ class Codec:
         if len(data) == 0:
             return None
 
-        orders: List[str] = data.split(self.SEMICOLON)
 
-        pre_order: List[str] = orders[0].split(self.COMMA)
-        in_order: List[str] = orders[1].split(self.COMMA)
-        post_order: List[str] = list()
+        pre_order: List[str] = data.split(self.COMMA)
+        in_order: List[str] = data.split(self.COMMA)
+        post_order: List[str] = data.split(self.COMMA)
 
-        return self.deserialize_pre_in(pre_order, in_order)
+        return self.deserialize_pre(pre_order)
 
-    def deserialize_pre_in(self, pre_order: List[str], in_order: List[str]) -> Optional[TreeNode]:
+    def deserialize_pre(self, pre_order: List[str]) -> Optional[TreeNode]:
 
         if len(pre_order) == 0:
             return None
-
+        value = pre_order.pop(0)
+        if value == "#":
+            return None
         root = TreeNode()
 
-        root.val = pre_order[0]
+        root.val = value
 
-        root_inorder_index = in_order.index(root.val)
-
-        root.left = self.deserialize_pre_in(pre_order[1: root_inorder_index + 1], in_order[:root_inorder_index])
-        root.right = self.deserialize_pre_in(pre_order[root_inorder_index + 1:], in_order[root_inorder_index + 1:])
-
+        root.left = self.deserialize_pre(pre_order)
+        root.right = self.deserialize_pre(pre_order)
         return root
 
     def traverse(self, root: Optional[TreeNode], pre_order: List[str], in_order: List[str], post_order: List[str]):
         if root is None:
+            pre_order.append(self.NULL)
+            in_order.append(self.NULL)
+            post_order.append(self.NULL)
             return
         pre_order.append(str(root.val))
         self.traverse(root.left, pre_order, in_order, post_order)
