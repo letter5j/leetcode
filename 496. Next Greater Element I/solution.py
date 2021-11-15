2a7ae9467c066af21df2077303f8ae6c571c2ba2
letter5j <seanletter5j@Gmail.com>
2021-11-15 23:36:20 +0800

    496. Next Greater Element I
    add solution

---
+++ 496. Next Greater Element I/solution.py
@@ -0,0 +1,33 @@
+from typing import List, Dict
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        next_element: Dict[int, int] = self.find_next_greater_element(nums2)
+
+        result: List[int] = list()
+        for num in nums1:
+            result.append(next_element[num])
+        return result
+
+    def find_next_greater_element(self, nums: List[int]) -> Dict[int, int]:
+
+        result: Dict[int, int] = dict()
+        stack: List[int] = list()
+        for i in range(len(nums) - 1, -1, -1):
+
+            while len(stack) != 0 and stack[len(stack) - 1] < nums[i]:
+                stack.pop(len(stack) - 1)
+
+            result[nums[i]] = stack[len(stack) - 1] if len(stack) != 0 else -1
+            stack.append(nums[i])
+        return result
+
+
+a = Solution()
+
+a.nextGreaterElement(
+    [4, 1, 2],
+    [1, 3, 4, 2]
+)
