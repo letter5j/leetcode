4764a7082b4dc8dd9fb5b4f77a1642eaa8b137b2
letter5j <seanletter5j@Gmail.com>
2021-11-17 23:52:43 +0800

    303. Range Sum Query - Immutable
    add solution

---
+++ 303. Range Sum Query - Immutable/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = list()
+        count = 0
+        for index in range(len(nums)):
+            self.nums.append(nums[index] + count)
+            count = count + nums[index]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.nums[right] - (self.nums[left - 1] if left > 0 else 0)
+
+obj = NumArray(
+[-2,0,3,-5,2,-1]
+)
+print(obj.sumRange(0,2))
