a6e01117235bd03d9b2c20ca46ac13ff6348bbcd
letter5j <seanletter5j@Gmail.com>
2021-11-26 00:02:21 +0800

    1143. Longest Common Subsequence
    add dp top-down solution

--- 1143. Longest Common Subsequence/solution.py
+++ 1143. Longest Common Subsequence/solution.py
@@ -2,6 +2,30 @@ from typing import Dict, List, Tuple
 
 
 class Solution:
+    def longestCommonSubsequence_top_down(self, text1: str, text2: str) -> int:
+
+        new_text1, new_text2 = self.remove_unnecessary_char(text1, text2)
+        memo: Dict[Tuple[int, int], int] = dict()
+        return self.dp_top_down(new_text1, new_text2, len(new_text1) - 1, len(new_text2) - 1, memo)
+
+    def dp_top_down(self, text1: str, text2: str, index1: int, index2: int, memo: Dict[Tuple[int, int], int]) -> int:
+
+        if index1 < 0 or index2 < 0:
+            return 0
+
+        if (index1, index2) in memo:
+            return memo[index1, index2]
+
+        if text1[index1] == text2[index2]:
+            result = self.dp_top_down(text1, text2, index1 - 1, index2 - 1, memo) + 1
+        else:
+            result = max(
+                self.dp_top_down(text1, text2, index1 - 1, index2, memo),
+                self.dp_top_down(text1, text2, index1, index2 - 1, memo)
+            )
+        memo[index1, index2] = result
+        return result
+
     def longestCommonSubsequence_bottom_up(self, text1: str, text2: str) -> int:
 
         new_text1, new_text2 = self.remove_unnecessary_char(text1, text2)
@@ -36,3 +60,10 @@ class Solution:
                 continue
             new_text1 = new_text1 + char
         return new_text1, new_text2
+
+
+s = Solution()
+s.longestCommonSubsequence_top_down(
+    "abcba",
+    "abcbcba"
+)
