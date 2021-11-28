57dac026966b8691fa40867968481f1800a42d93
letter5j <seanletter5j@Gmail.com>
2021-11-29 00:41:29 +0800

    516. Longest Palindromic Subsequence
    add dp top-down solution

--- 516. Longest Palindromic Subsequence/solution.py
+++ 516. Longest Palindromic Subsequence/solution.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Tuple
 
 
 class Solution:
@@ -16,3 +16,25 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
         return dp[0][s_length - 1]
+
+    def longestPalindromeSubseq_top_down(self, s: str) -> int:
+        memo: Dict[Tuple[int, int], int] = dict()
+        return self.dp_top_down(s, 0, len(s) - 1, memo)
+
+    def dp_top_down(self, s: str, start_index: int, end_index: int, memo: Dict[Tuple[int, int], int]) -> int:
+
+        if start_index == end_index:
+            return 1
+        if start_index > end_index:
+            return 0
+        if (start_index, end_index) in memo:
+            return memo[start_index, end_index]
+
+        if s[start_index] == s[end_index]:
+            result = self.dp_top_down(s, start_index + 1, end_index - 1, memo) + 2
+        else:
+            result = max(self.dp_top_down(s, start_index + 1, end_index, memo),
+                         self.dp_top_down(s, start_index, end_index - 1, memo))
+        memo[start_index, end_index] = result
+
+        return result
