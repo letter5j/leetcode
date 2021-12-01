08805d4e3eb917b871034c25c442dc5791998897
letter5j <seanletter5j@Gmail.com>
2021-12-01 21:43:16 +0800

    518. Coin Change 2
    add dp bottom-up and compress and test

--- 518. Coin Change 2/solution.py
+++ 518. Coin Change 2/solution.py
@@ -26,3 +26,32 @@ class Solution:
 
         memo[index, amount] = result
         return result
+
+    def change_bottom_up(self, amount: int, coins: List[int]) -> int:
+        coin_length = len(coins)
+        dp: List[List[int]] = [[0] * (amount + 1) for _ in range(coin_length + 1)]
+
+        for coin in range(1, coin_length + 1):
+            dp[coin][0] = 1
+
+        for coin in range(1, coin_length + 1):
+            for sub_amount in range(1, amount + 1):
+
+                if coins[coin - 1] > sub_amount:
+                    dp[coin][sub_amount] = dp[coin - 1][sub_amount]
+                else:
+                    dp[coin][sub_amount] = dp[coin - 1][sub_amount] + \
+                                           dp[coin][sub_amount - coins[coin - 1]]
+
+        return dp[coin_length][amount]
+
+    def change_bottom_up_compress(self, amount: int, coins: List[int]) -> int:
+        coin_length = len(coins)
+        dp: List[int] = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in range(coin_length):
+            for sub_amount in range(1, amount + 1):
+                if sub_amount >= coins[coin]:
+                    dp[sub_amount] = dp[sub_amount] + dp[sub_amount - coins[coin]]
+
+        return dp[amount]
--- 518. Coin Change 2/test_solution.py
+++ 518. Coin Change 2/test_solution.py
@@ -20,6 +20,34 @@ class TestSolution(unittest.TestCase):
             self.solution.change_top_down(amount=10, coins=[10])
         )
 
+    def test_change_bottom_up(self):
+        self.assertEqual(
+            4,
+            self.solution.change_bottom_up(amount=5, coins=[1, 2, 5])
+        )
+        self.assertEqual(
+            0,
+            self.solution.change_bottom_up(amount=3, coins=[2])
+        )
+        self.assertEqual(
+            1,
+            self.solution.change_bottom_up(amount=10, coins=[10])
+        )
+
+    def test_change_bottom_up_compress(self):
+        self.assertEqual(
+            4,
+            self.solution.change_bottom_up_compress(amount=5, coins=[1, 2, 5])
+        )
+        self.assertEqual(
+            0,
+            self.solution.change_bottom_up_compress(amount=3, coins=[2])
+        )
+        self.assertEqual(
+            1,
+            self.solution.change_bottom_up_compress(amount=10, coins=[10])
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
