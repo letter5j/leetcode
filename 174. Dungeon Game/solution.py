4eaea3982308a9004171f34b983878c6e4789859
letter5j <seanletter5j@Gmail.com>
2021-12-08 22:12:44 +0800

    174. Dungeon Game
    add dp bottom-up compress solution

--- 174. Dungeon Game/solution.py
+++ 174. Dungeon Game/solution.py
@@ -47,3 +47,25 @@ class Solution:
                         dp[row_index][column_index] = 1
 
         return dp[0][0]
+
+    def calculateMinimumHP_bottom_up_compress(self, dungeon: List[List[int]]) -> int:
+
+        row_length = len(dungeon)
+        column_length = len(dungeon[0])
+        dp: List[int] = [float("inf")] * column_length
+        prev: List[int] = [float("inf")] * column_length
+
+        for row_index in range(row_length - 1, -1, -1):
+
+            for column_index in range(column_length - 1, -1, -1):
+                if (row_index == row_length - 1) and (column_index == column_length - 1):
+                    dp[column_index] = 1 if dungeon[row_index][column_index] > 0 else abs(
+                        dungeon[row_index][column_index]) + 1
+                else:
+                    dp[column_index] = min(prev[column_index],
+                                           dp[column_index + 1] if column_index < column_length - 1 else float("inf")) - \
+                                       dungeon[row_index][column_index]
+                    if dp[column_index] <= 0:
+                        dp[column_index] = 1
+                prev[column_index] = dp[column_index]
+        return dp[0]
